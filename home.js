e03d71181195cf30f3c13ab1c6efc084fe000d5e
RobotNatchos <robot6895games@gmail.com>
2023-08-26 22:14:34 -0400

    update

--- home.js
+++ home.js
@@ -13,9 +13,6 @@ const observer = new IntersectionObserver((entries) => {
       if(entry.isIntersecting) {
         entry.target.classList.add('show')
       }
-      else {
-        entry.target.classList.remove('show')
-      }
   });
 });
 
