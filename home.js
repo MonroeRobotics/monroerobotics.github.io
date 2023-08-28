4bb83e73a2632cf38dce7218c1ecc03e91706d25
RobotNatchos <47341210+RobotNatchos@users.noreply.github.com>
2023-08-28 16:22:23 -0400

    Update

--- home.js
+++ home.js
@@ -4,7 +4,7 @@ function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-100px";
+    document.getElementById("navbar").style.top = "var(--navHeightUp)";
   }
 } 
 
